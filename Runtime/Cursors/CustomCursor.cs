7ad9b1dd7d520445d641bed010408fe2694e2a3d
Sebastien Berton <sebasti1.berton@gmail.com>
2026-06-13 15:57:53 +0900

    added info on CustomCursor

--- Runtime/Cursors/CustomCursor.cs
+++ Runtime/Cursors/CustomCursor.cs
@@ -6,6 +6,12 @@ namespace Bakery
     public class CustomCursor : MonoBehaviour
     {
         [SerializeField] private CursorType _cursor;
+
+        void OnValidate()
+        {
+            if (_cursor == null)
+                Debug.LogWarning($"Cursor is not set for {gameObject.name} in {GetType().Name}");
+        }
         public CursorType Cursor
         {
             get => _cursor; set => _cursor = value;
