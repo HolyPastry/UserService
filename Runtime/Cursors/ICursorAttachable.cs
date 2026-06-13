b916b4e9e5721eaaaf383692e5f410718497fc9d
Sebastien Berton <sebasti1.berton@gmail.com>
2026-06-13 15:45:52 +0900

    Made function not mandatory

--- Runtime/Cursors/ICursorAttachable.cs
+++ Runtime/Cursors/ICursorAttachable.cs
@@ -5,8 +5,8 @@ namespace Bakery
     public interface ICursorAttachable
     {
         void UpdatePosition(Vector2 position);
-        bool IsAttached { get; }
-        void Attach();
-        void Detach();
+        bool IsAttached { get => false; }
+        void Attach() { }
+        void Detach() { }
     }
 }
